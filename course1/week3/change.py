f2d4e2e445d7c6982518add5260e0d8571ad4726
Alexander Onbysh <alexandr.onbysh@ring.com>
2018-06-16 14:20:06 +0300

    week 3

---
+++ course1/week3/change.py
@@ -0,0 +1,11 @@
+# Uses python3
+import sys
+
+def get_change(m):
+    t, r = m // 10, m % 10
+    f, r = r // 5, r % 5
+    return t + f + r
+
+if __name__ == '__main__':
+    m = int(sys.stdin.read())
+    print(get_change(m))
---
+++ course1/week3/fractional_knapsack.py
@@ -0,0 +1,23 @@
+# Uses python3
+import sys
+
+def get_optimal_value(capacity, weights, values):
+    ratio = [v / w for v, w in zip(values, weights)]
+    s = sorted(zip(ratio, values, weights), reverse=True)
+    total_value = 0
+    capacity_left = capacity
+    for ratio, value, weight in s:
+        take = min(capacity_left, weight)
+        capacity_left -= take
+        total_value += take / weight * value
+        
+    return total_value
+
+
+if __name__ == "__main__":
+    data = list(map(int, sys.stdin.read().split()))
+    n, capacity = data[0:2]
+    values = data[2:(2 * n + 2):2]
+    weights = data[3:(2 * n + 2):2]
+    opt_value = get_optimal_value(capacity, weights, values)
+    print("{:.10f}".format(opt_value))
